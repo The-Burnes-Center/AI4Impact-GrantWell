b1f05ccae08655820fdd03080599a11713e5944e
anuttan <anjith16@gmail.com>
2026-05-05 12:42:33 -0400

    Update Lambda function permissions to include StopExecution action

--- lib/chatbot-api/functions/functions.ts
+++ lib/chatbot-api/functions/functions.ts
@@ -664,7 +664,7 @@ export class LambdaFunctionStack extends cdk.Stack {
     dispatcherFunction.addToRolePolicy(
       new iam.PolicyStatement({
         effect: iam.Effect.ALLOW,
-        actions: ["states:DescribeExecution", "states:ListExecutions"],
+        actions: ["states:DescribeExecution", "states:ListExecutions", "states:StopExecution"],
         resources: ["*"],
       })
     );
